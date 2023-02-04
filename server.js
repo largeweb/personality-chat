80eeca34a3052ef2266da0689afc7a774d244e40
matt <mat@unc.edu>
2023-02-04 11:57:10 -0500

    commit

--- server.js
+++ server.js
@@ -43,6 +43,7 @@ app.use(require('body-parser').json());
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET');
+  res.header('Access-Control-Allow-Methods', 'POST');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
